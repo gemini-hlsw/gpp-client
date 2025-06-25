a94acc4f185f483cbbe3a6e99edfdb4ab78066ea
Dan Avner <dan.avner@noirlab.edu>
2025-06-25 13:09:44 -0700

    GPC-37: Fix issue with workflow and set no_args_is_help to false.

--- tests/gpp_client/cli/test_cli.py
+++ tests/gpp_client/cli/test_cli.py
@@ -12,15 +12,3 @@ class TestCLI:
         assert "config" in result.stdout
         assert "target" in result.stdout
         assert "site" in result.stdout
-
-    def test_help_no_flag(self, cli_runner):
-        """Verify the help screen is shown for just typing the CLI."""
-        result = cli_runner.invoke(app)
-        assert result.exit_code == 0
-        assert "pnote" in result.stdout
-        assert "cfp" in result.stdout
-        assert "program" in result.stdout
-        assert "config" in result.stdout
-        assert "target" in result.stdout
-        assert "obs" in result.stdout
-        assert "site" in result.stdout
