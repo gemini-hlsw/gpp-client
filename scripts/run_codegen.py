a76a9bf74df0470f390a6a7cf70fce802dedc453
Dan Avner <dan.avner@noirlab.edu>
2025-12-30 10:16:08 -0800

    SC-7496: Fix name of Flamingos2 long slit.
    - Correct bug decoding stderr.

--- scripts/run_codegen.py
+++ scripts/run_codegen.py
@@ -166,7 +166,7 @@ def main(
                 if backup_dir.exists():
                     backup_dir.rename(api_dir)
 
-                fail(exc.stderr.decode())
+                fail(exc.stderr)
 
         typer.echo("Codegen completed successfully")
 
