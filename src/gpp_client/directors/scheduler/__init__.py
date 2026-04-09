9a0d01af48cafa0fe157a8d59fd92dd564ecf9ef
Dan Avner <dan.avner@noirlab.edu>
2026-04-09 13:42:50 -0700

    Remove all code related to the old GPPClient implementation

--- src/gpp_client/directors/scheduler/__init__.py
+++
@@ -1,3 +0,0 @@
-from .scheduler import SchedulerDirector
-
-__all__ = ["SchedulerDirector"]
--- src/gpp_client/subscribers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .observation import ObservationSubscriber
-from .program import ProgramSubscriber
-from .target import TargetSubscriber
-
-__all__ = ["ObservationSubscriber", "ProgramSubscriber", "TargetSubscriber"]
