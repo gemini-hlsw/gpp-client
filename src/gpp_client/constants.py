21e61f97e22496821c0c73a4fe4b847a51c091ca
Dan Avner <dan.avner@noirlab.edu>
2026-04-09 13:44:56 -0700

    Refactor the GPPClient

---
+++ src/gpp_client/constants.py
@@ -0,0 +1,21 @@
+"""
+Constants for the installed GPP client package.
+"""
+
+APP_NAME = "gpp-client"
+"""Directory name for the application."""
+
+CONFIG_FILE_NAME = "config.toml"
+"""Configuration file name for the application."""
+
+DEVELOPMENT_URL = "https://lucuma-postgres-odb-dev.herokuapp.com"
+"""GraphQL URL for the development environment."""
+
+PRODUCTION_URL = "https://lucuma-postgres-odb-production.herokuapp.com"
+"""GraphQL URL for the production environment."""
+
+TOKEN_ENV_VAR = "GPP_TOKEN"
+"""Environment variable name for the GPP token."""
+
+DEVELOPMENT_TOKEN_ENV_VAR = "GPP_DEVELOPMENT_TOKEN"
+"""Environment variable name for the GPP development token."""
